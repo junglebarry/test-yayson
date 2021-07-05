def822c6b0c0e5dbf1b8d4dd8356fde6eaa02f12
David Brooks <david.brooks@talis.com>
2021-07-05 10:55:06 +0100

    README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # test-yayson
+
+Talis users with `persona-token local`, do this:
+
+```bash
+npm run start
+```
+
+If you don't, do this:
+
+```bash
+PERSONA_TOKEN=<a Persona token capable of accessing the TARL local API> node index.js
+```
--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const port = 3000
 
 const store = new yayson.Store()
 
-const token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImxvY2FsLWp3ayJ9.eyJqdGkiOiI3NWI0NGVhMWVlNDhlNGM2ZTgyOGZlYTIwZGU3MDY5MDY5NTdmOTQ1IiwiaWF0IjoxNjI1NDc3Mjk2LCJleHAiOjE2MjU0ODMyOTYsImF1ZCI6InByaW1hdGUiLCJzdWIiOiJwcmltYXRlIiwic2NvcGVzIjpbInN1IiwicHJpbWF0ZSJdfQ.GdIpMIy-6DoZTXpIMNfP15J9bXXGl3DRYC_G9oNrwjXfE8Tty0liqB-hfs1DSh2OpowisDl2KGOaHCa78bmQk1dNntCpN6WgDVsRdr7Gvtf6WrfxP5KbH1askR45m8iE3ADhyU1E9B9uRZ6cls-BQMCrJEUrLJY4VfS0Nop83zWTUDU2y96NahhJurqaQRlgOiA5pKFyfzTDZ0bLPBKdvHMqh7mCOFM-uI32X4MvVn3_uX3w6Ik7LWS5RkDKziqe9k_UVHCf6u4iCEwC0AaNTwQOko0AlZE4yjQSKbPc3V8m0ji93EhIop3WYhcmuHA2lY-2m984XfZyd1TefJ3hdg`;
+const token = process.env.PERSONA_TOKEN;
+
+if (!token) {
+  throw new Error('Please specify a valid PERSONA_TOKEN')
+}
 
 app.get('/', async (req, res) => {
   try {
